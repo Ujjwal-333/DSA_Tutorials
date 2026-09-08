c66c8b4f1a0ddc51c947d1e64f052fdc2b68e182
Ujjwal <ujjwalpandey569@gmail.com>
2026-09-08 19:46:05 +0530

    Second commit

---
+++ Sorting/bubble_sort.js
@@ -0,0 +1,19 @@
+let arr = [39, 90, 89, 67, 45, 32, 45, 97];
+
+function bubbleSort(arr) {
+    for (let i = 0; i < arr.length - 1; i++) {
+        for (let j = 0; j < arr.length - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+    return arr;
+}
+
+let result = bubbleSort(arr);
+
+console.log(result);
